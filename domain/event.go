f7a448d310307b0893311149fd47522250525fe5
Danila Stivrinsh <danilaridzhi@gmail.com>
2023-11-07 21:41:15 +0300

    upd

--- domain/event.go
+++ domain/event.go
@@ -2,12 +2,27 @@ package domain
 
 const (
 	EventGameCreated EventId = iota + 1
+	EventRoomCreated
+	EventRoomUpdated
+	EventRoomDeleted
 )
 
 type EventId int
 
 type GameCreatedPayload struct {
 	Game *Game
+}
+
+type RoomCreatedPayload struct {
+	Room *Room
+}
+
+type RoomDeletedPayload struct {
+	Room *Room
+}
+
+type RoomUpdatedPayload struct {
+	Room *Room
 }
 
 type RoomCreated struct {
